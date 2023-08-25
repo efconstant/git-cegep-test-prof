b22d1831736b71ec6e1307fc22dfbdc1091b2129
efconstant <78762241+efconstant@users.noreply.github.com>
2023-08-24 22:33:19 -0400

    Add files via upload

---
+++ ClassKid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractStatic
+{
+    internal class ClassKid : AbstractClass
+    //internal class ClassKid : StaticClass
+    {
+        /*public override bool IsTrue(bool isTrue)
+        {
+            return !isTrue;
+        }*/
+    }
+}
